af9dcc2f1cb10fc72ee2afff55f5b08083190533
Rafael <soratto@alunos.utfpr.edu.br>
2019-06-09 19:35:36 -0300

    update get report

--- back-end-app/src/repositories/report-repository.js
+++ back-end-app/src/repositories/report-repository.js
@@ -7,7 +7,7 @@ const Report =  mongoose.model('Report');
 exports.get = async () => {  
     const res = await Report.find({ 
             status:0  
-        }, 'lat lng '); 
+        }, 'lat lng title description'); 
     return res;    
 }  
 
