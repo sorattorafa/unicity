98d6422d799b3b63dc848a71ee96717487413cfe
Darlan Andrade <flame2br@gmail.com>
2019-06-16 22:20:51 -0300

    updated status and date display

--- front-end/src/components/report/view-report.js
+++ front-end/src/components/report/view-report.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import { Descriptions, Divider, Typography, Layout, Badge, Icon } from 'antd';
+import { Descriptions, Divider, Typography, Layout, Badge, Icon, Row } from 'antd';
 import "antd/dist/antd.css";
 
 import NavBar from '../../components/navbar/navbar';
@@ -11,65 +11,81 @@ const { Content } = Layout;
 
 export default class ViewReport extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
-        this.state = {   
-        lat: '',
-        lng: '',
-        title: '',
-        cep: '',
-        street: '',
-        number: '',
-        description: '',
-        createDate: '',
-        number_of_denunciations: '',
-        number_of_supports: '',
-        status: ''
-        }
+        this.state = {  
+            lat:'',  
+            lng:'',
+            title: '',
+            cep: '',
+            street: '',  
+            number: '', 
+            description: '',
+            createDate: '',
+            number_of_denunciations: '',
+            number_of_supports: '',
+            status: '',
+            category: ''
+          }
     }
 
     componentDidMount() {
         axios.get('/reports/'+this.props.match.params.id)
-            .then(response => {
-                this.setState({
-                    
-                    lat: response.data.lat, 
-                    lng: response.data.lng,
-                    title: response.data.title, 
-                    cep: response.data.cep, 
-                    street: response.data.street, 
-                    number: response.data.number, 
-                    description: response.data.description, 
-                    createDate: response.data.createDate,  
-                    number_of_denunciations: response.data.number_of_denunciations, 
-                    number_of_supports: response.data.number_of_supports,
-                    status: response.data.status                  
-                })
+        .then(response => {
+            this.setState({  
+                lat: response.data.lat, 
+                lng: response.data.lng,
+                title: response.data.title, 
+                cep: response.data.cep, 
+                street: response.data.street, 
+                number: response.data.number, 
+                description: response.data.description, 
+                createDate: response.data.createDate,  
+                number_of_denunciations: response.data.number_of_denunciations, 
+                number_of_supports: response.data.number_of_supports,
+                status: response.data.status,
+                category: response.data.category
             })
-            .catch(function(error) {
-                console.log(error)
-            })
+        })
+        .catch(function(error) {
+            console.log(error)
+        })
     }
 
     render() {
 
-        return(
+        const ReportStatus = this.state.status;
+        const FormatDate = this.state.createDate.split("T")[0];
 
-            <Layout style = {{ minHeight: '100vh' }}>
+        function StatusCheck(){
+            if (ReportStatus === 0) {
+                return <Badge status="processing" text="Aberto" />;
+            } else if (ReportStatus === 1) {
+                return <Badge status="warning" text="Em Resolução" />;
+            } else {
+                return <Badge status="success" text="Resolvido" />;
+            }
+        }
+
+        return(
+            <Layout style = {{ minHeight: '100vh', width: '100%' }}>
                 <NavBar />
 
                 <Layout>
+                    <LateralMenu pagina = "createadminuser" />
                     <Content className = "contentLayoutForm" style = {{ padding: "30px 20px 0px 20px" }} >
                         <Title className = "titleForm" level={1}> {this.state.title} </Title>
                         <Divider className = "dividerForm" />
                             <div>
                                 <Descriptions
-                                    title={this.state.title}
                                     border
                                     column={{ xxl: 3, xl: 3, lg: 3, md: 2, sm: 2, xs: 1 }}
                                 >
-                                    <Descriptions.Item label="Descrição" span={3}>{this.state.description}</Descriptions.Item>
+                                    <Descriptions.Item label="Descrição" span={3}>
+                                        {this.state.description}
+                                        <br />
+                                    </Descriptions.Item>
                                     <Descriptions.Item label="Endereço">
                                         Rua: {this.state.street}
                                         <br />
@@ -85,10 +101,10 @@ export default class ViewReport extends Component {
                                         <br />
                                     </Descriptions.Item>
                                     <Descriptions.Item label="Status">
-                                       <Badge status="processing" text="Ativo" />
-                                       <br />
-                                       Data de Criação: {this.state.createDate}
-                                       <br />
+                                        <StatusCheck ReportStatus={this.state.status} />,
+                                        <br />
+                                         Data de Criação: {FormatDate}
+                                         <br />
                                     </Descriptions.Item>
                                     <Descriptions.Item label="Apoiadores" span={3}>
                                         {this.state.number_of_supports} <Icon type="like" />
@@ -99,7 +115,6 @@ export default class ViewReport extends Component {
                                 </Descriptions>
                             </div>
                     </Content>
-
                 </Layout>
             </Layout>
         )
